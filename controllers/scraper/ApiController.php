1720e9d15ac8051ee1ccb9daf1b3c00682329203
raoul2000 <manu34@free.fr>
2017-08-05 20:08:08 +0200

    add proxy api for scraper

--- controllers/scraper/ApiController.php
+++ controllers/scraper/ApiController.php
@@ -6,6 +6,7 @@ use Yii;
 use yii\db\ActiveQuery;
 use yii\data\ActiveDataProvider;
 use yii\db\Query;
+use yii\httpclient\Client;
 
 class ApiController extends \yii\rest\ActiveController
 {
@@ -32,5 +33,30 @@ class ApiController extends \yii\rest\ActiveController
       return true;
     }
 
-  
+    public function actionProxy() {
+      $client = new Client();
+      $response = $client->createRequest()
+        ->setMethod('post')
+        ->setFormat(Client::FORMAT_JSON)
+        ->setUrl('https://raoul2000.herokuapp.com/scraper')
+        ->setData([
+          'url' => 'http://ass-team.fr/app/web/index.php?r=site/post-index',
+          //'selector' => ''
+          'selector' => '.post-container h3'
+        ])
+        ->send();
+        if ($response->isOk) {
+          return [
+            'success' => true,
+            'statusCode' => $response->getStatusCode(),
+            'data' => $response->getData()
+          ];
+        } else {
+          return [
+            'success' => false,
+            'statusCode' => $response->getStatusCode()
+          ];
+        }
+
+    }
 }
