ec4284ab06552ebc6d681851fcadd28b34a71173
raoul2000 <manu34@free.fr>
2017-08-05 19:38:06 +0200

    set up base rest api for scraper

--- controllers/scraper/ApiController.php
+++ controllers/scraper/ApiController.php
@@ -3,13 +3,34 @@
 namespace app\controllers\scraper;
 
 use Yii;
+use yii\db\ActiveQuery;
+use yii\data\ActiveDataProvider;
+use yii\db\Query;
 
 class ApiController extends \yii\rest\ActiveController
 {
     public $modelClass = 'app\models\scraper\ScraperModel';
 
+    public function actions()
+    {
+      $actions = parent::actions();
+      // customize the data provider preparation with the "prepareDataProvider()" method
+      $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+      return $actions;
+    }
+
+    public function prepareDataProvider()
+    {
+      $modelClass = $this->modelClass;
+      return new ActiveDataProvider([
+          'query' => (new Query())->select(['id', 'name'])->from($modelClass::tableName())
+      ]);
+    }
+
     public function checkAccess($action, $model = null, $params = [])
     {
       return true;
     }
+
+  
 }
